c47f0959992a4844df4fd42146f3a1b0a097e070
Datta Soat <dattasot2025@gmail.com>
2021-09-06 02:21:40 +0530

    main page

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import HomePage from './homepage.component'
+import './App.css';
+
+function App() {
+  return (
+    
+    <HomePage />
+
+  );
+}
+
+export default App;
---
+++ app/src/homepage.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+ const HomePage=()=>{
+    return (
+        <div className="homePage">
+            <div className="directoryMenu">
+                <div className="menuItem">
+                    <div className="content">
+                        <h1 className="title">HAT</h1>
+                        <span className="subTitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menuItem">
+                    <div className="content">
+                        <h1 className="title">HAT</h1>
+                        <span className="subTitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menuItem">
+                    <div className="content">
+                        <h1 className="title">HAT</h1>
+                        <span className="subTitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menuItem">
+                    <div className="content">
+                        <h1 className="title">HAT</h1>
+                        <span className="subTitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menuItem">
+                    <div className="content">
+                        <h1 className="title">HAT</h1>
+                        <span className="subTitle">SHOP NOW</span>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
